09e9bba3f578c35a0288de4ee9d44c4e5c1ff781
Duy Anh <Duy Anh>
2018-07-04 00:30:05 +0700

    Just testing github

--- app/src/main/java/com/tapspeedcounter/admin/myapplication/TapActivity.java
+++ app/src/main/java/com/tapspeedcounter/admin/myapplication/TapActivity.java
@@ -193,7 +193,7 @@ public class TapActivity extends AppCompatActivity {
             }
         });
     }
-    //The default countdown timer sucks, so I have to create a customized one
+    //The default countdown timer sucks, so I have to create a customized one.
     public BetterCountDownTimer mainCounter(final int interval) {
         speedValue = getSpeed()+"";
         return new BetterCountDownTimer(interval, 100, 1000) {
